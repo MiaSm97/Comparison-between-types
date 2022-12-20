a170d297d76396d117fdab0057ffe890ea4b571d
Mia Smaldone <eufosm@libero.it>
2022-12-20 10:58:20 +0100

    Esercizio #3

---
+++ src/main/kotlin/Start.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val num1 = 5
+    val num2 = 4.5F
+
+    println("Is number 1 equal to number 2? ${num1.toFloat() == num2}")
+    println("Is number 1 greater than number 2? ${num1 > num2}")
+    println("Is number 1 minor than number 2? ${num1 < num2}")
+}
+
+/*Compare values of 2 variables of different type (e.g. float and integer or string and float)
+and print the result using string templates
+ */
